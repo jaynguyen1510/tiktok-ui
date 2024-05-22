0e6fbbb76d91ac3d29efdaeaf997bb8e505a10e0
jaynguyen1510 <ngnhatnam1510@gmail.com>
2024-05-22 15:09:32 +0700

    new3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,33 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
+import images from "~/asset/images";
 
 const cx = classNames.bind(styles);
+
+console.log(images.logo);
 function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        {/*logo */}
-        {/*search */}
+        <img src={images.logo} alt="tiktok" />
+
+        <div className={cx("search")}>
+          <input placeholder="Tìm kiếm video" spellCheck={false} />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx("acton")}></div>
       </div>
     </header>
   );
