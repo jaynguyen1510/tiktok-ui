c342648adf3d20e739ef4dabb980d34b6aba7209
jaynguyen1510 <ngnhatnam1510@gmail.com>
2024-05-25 12:31:09 +0700

    new3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,32 +1,57 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import Tippy from "@tippyjs/react/headless";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Wrapper as PoperWarper } from "~/components/Poper";
 import {
   faCircleXmark,
   faMagnifyingGlass,
   faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
 import images from "~/asset/images";
+import { useEffect, useState } from "react";
 
 const cx = classNames.bind(styles);
 
 console.log(images.logo);
+
 function Header() {
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([1, 2, 3]);
+    }, 3000);
+  }, []);
+
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
         <img src={images.logo} alt="tiktok" />
+        <Tippy
+          interactive
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+              <PoperWarper>
+                <h4 className={cx("search-title")}>Accounts</h4>
+              </PoperWarper>
+            </div>
+          )}
+        >
+          <div className={cx("search")}>
+            <input placeholder="Tìm kiếm video" spellCheck={false} />
+            <button className={cx("clear")}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
 
-        <div className={cx("search")}>
-          <input placeholder="Tìm kiếm video" spellCheck={false} />
-          <button className={cx("clear")}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </button>
-          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
-          <button className={cx("search-btn")}>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
-        </div>
+            <button className={cx("search-btn")}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
         <div className={cx("acton")}></div>
       </div>
     </header>
